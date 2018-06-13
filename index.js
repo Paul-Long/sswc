a381735fc7ee964df39afe5e98ea3782f2a65699
paul <7328194@163.com>
2018-06-13 18:04:18 +0800

    init

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
 import Worker from 'sharedworker-loader!./worker.js';
 
 type OptParams = {
-  clientId: string,
+  heatTimer: number,
 };
 
 class SharedWorker {
   _worker = null;
   _port = null;
   _option = {};
+  _topics = new Map();
+  _timer = 3 * 1000;
 
   constructor(option: OptParams) {
-    if (!option.clientId) {
-      throw new Error(`[clientId] : shared worker clientId is ${option.clientId}`);
+    const { heatTimer } = option;
+    if (heatTimer) {
+      this._timer = heatTimer;
     }
-
     this._option = { ...option };
     this._worker = new Worker('./worker.js');
     this._port = this._worker.port;
-    this._port.onerror = this.onError;
-    this._port.onmessage = this.onMessage;
+    this._port.onerror = ::this.onError;
+    this._port.onmessage = ::this.onMessage;
     this._port.start();
   }
 
@@ -27,37 +29,82 @@ class SharedWorker {
   }
 
   onMessage(event) {
-    console.log(event, event.data);
+    const {
+      action,
+    } = event.data || {};
+    if (action === 'connect') {
+      this.onConnected(event);
+    } else {
+      const handler = this._topics.get(action);
+      if (handler) {
+        handler(event.data);
+      }
+    }
   }
 
   onConnected(event) {
-
+    if (this._topics.has('connect')) {
+      const handler = this._topics.get('connect');
+      if (typeof handler === 'function') {
+        handler();
+      }
+    }
+    this.keep();
+    setInterval(() => {
+      this.keep();
+    }, this._timer);
   }
 
-  onClose(event) {
-
+  keep() {
+    this._port.postMessage({
+      action: 'heat',
+    });
   }
 
   connect() {
     this._port.postMessage({
       action: 'connect',
+      timer: this._timer + 1000,
     });
   }
 
   emit(topic, message) {
-
+    this._port.postMessage({
+      action: 'emit',
+      topic,
+      message,
+    });
   }
 
   subscribe(topic) {
-
+    this._port.postMessage({
+      action: 'subscribe',
+      topic,
+    });
   }
 
-  on(topic) {
+  on(topic, callback) {
+    if (topic === 'connect') {
+      this.connect();
+    } else {
+      this.subscribe(topic);
+    }
+    if (typeof callback === 'function') {
+      this._topics.set(topic, callback);
+    }
+  }
 
+  join(channel) {
+    this._port.postMessage({
+      action: 'join',
+      channel,
+    });
   }
 
   close() {
-
+    this._port.postMessage({
+      action: 'close',
+    });
   }
 }
 
--- worker.js
+++ worker.js
@@ -1,41 +1,193 @@
+import io from 'socket.io-client';
+
 class Worker {
-  constructor() {
+  _clients = new Set();
+  _socket;
+  _num = 1;
+  _regTopics = new Set();
+  _connected = false;
 
+  connect(client) {
+    this._clients.add(client);
+    console.info(`client count : [${this._clients.size}]`);
+    if (this._socket && this._connected) {
+      client.connected();
+    } else {
+      this._socket = io('', {
+        transports: ['polling', 'websocket']
+      });
+      this._socket.on('connect', () => {
+        this._connected = true;
+        this._clients.forEach(c => c.connected());
+      });
+      this._socket.on('disconnect', () => {
+        this._connected = false;
+        this._clients.forEach(c => c.disconnect());
+        this._clients.clear();
+      });
+      this._socket.on('reconnecting', (number) => {
+        this._clients.forEach(c => c.reconnecting(number));
+      });
+    }
   }
 
-  connect() {
-
+  add() {
+    return this._num++;
   }
 
-  emit() {
-
+  on(topic) {
+    if (!this._regTopics.has(topic)) {
+      this._regTopics.add(topic);
+      if (this._socket && this._connected) {
+        this._socket.on(topic, (message) => {
+          this._clients.forEach(client => {
+            if (client._topics.has(topic)) {
+              const handler = client._topics.get(topic);
+              if (typeof handler === 'function') {
+                handler(message);
+              }
+            }
+          });
+        });
+      }
+    }
   }
 
-  regTopic() {
-
+  emit(topic, message) {
+    if (this._socket && this._connected) {
+      this._socket.emit(topic, message);
+    }
   }
 
-  disconnect() {
-
+  disconnect(client) {
+    this.close(client);
+    console.log(`disconnect client [${client._num}], current client count:  [${this._clients.size}]`);
   }
 
-  reconnecting() {
-
+  close(client) {
+    if (this._socket && this._connected) {
+      this._socket.emit('close', Array.from(client._channels));
+    }
+    this._clients.delete(client);
   }
 }
 
 const worker = new Worker();
 
-self.addEventListener('connect', function (event) {
-  const client = event.ports[0];
-  console.log('client connect', client);
-  client.addEventListener('message', function (e) {
-    let action = event.data.action;
-    if (action === '') {
-      client.postMessage({
-        action: 'connect',
-        data: 'connect success'
-      });
+type ClientOpt = {
+  client: any,
+}
+
+class Client {
+  _timer;
+  _client;
+  _num;
+  _time = 3 * 1000;
+  _topics = new Map();
+  _channels = new Set();
+  _connected = false;
+
+  constructor(option: ClientOpt) {
+    this._client = option.client;
+    ::this.init();
+  }
+
+  init() {
+    this._client.addEventListener('message', ::this.onMessage);
+    this._client.start();
+  }
+
+  onMessage(event) {
+    const { action, timer, channel } = event.data;
+    switch (action) {
+      case 'connect':
+        this._num = worker.add();
+        this._time = timer;
+        break;
+      case 'subscribe':
+        this.subscribe(event);
+        break;
+      case 'emit':
+        this.emit(event);
+        break;
+      case 'heat':
+        this.heat();
+        break;
+      case 'close':
+        worker.close(this);
+        break;
+      case 'join':
+        this._channels.add(channel);
+        worker.emit('join', channel);
+        break;
     }
-  });
-});
+  }
+
+  subscribe = (event) => {
+    const {
+      topic,
+    } = event.data || {};
+    if (!this._topics.has(topic)) {
+      this._topics.set(topic, (message) => {
+        this._client.postMessage({
+          action: topic,
+          message,
+        });
+      });
+      worker.on(topic);
+    }
+  };
+
+  emit = (event) => {
+    const { topic, message } = event.data || {};
+    worker.emit(topic, message);
+  };
+
+  heat = () => {
+    this.clearTimer();
+    this._timer = setTimeout(() => {
+      this.clearTimer();
+      this._client.close();
+      worker.disconnect(this);
+    }, this._time);
+  };
+
+  clearTimer() {
+    if (this._timer) {
+      clearTimeout(this._timer);
+      this._timer = null;
+    }
+  }
+
+  connected() {
+    this._connected = true;
+    this._client.postMessage({
+      action: 'connect'
+    });
+  }
+
+  disconnect() {
+    this._connected = false;
+    this._client.postMessage({
+      action: 'disconnect'
+    });
+  }
+
+  reconnecting(number) {
+    this._client.postMessage({
+      action: 'reconnecting',
+      number,
+    });
+  }
+
+  start() {
+    this._client.start();
+  }
+}
+
+onconnect = (event) => {
+  const client = event.ports[0];
+  worker.connect(new Client({
+    client,
+  }));
+};
